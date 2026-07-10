e077608a41b6dabeabd95c4ce0dddf02fdd19b6f
Alexandre-AGAMIN <alexandrepm2810@gmail.com>
2026-07-10 15:17:58 -0300

    ajsute de nome de bucket

--- infra/terraform/cloud/backend.tf
+++ infra/terraform/cloud/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "fiap-repairshop"
+    bucket  = "fiap-repairshop2"
     key     = "terraform-config/tfstate/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
